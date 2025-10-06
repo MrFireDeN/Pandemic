ba26fda1640d7d14b01b09cf84052848ca3c7de4
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-10-06 09:58:46 +0300

    feat: добавлен класс городов

--- game.py
+++ game.py
@@ -1,4 +1,8 @@
-﻿class PandemicGame:
+﻿import data.cities
+
+class PandemicGame:
+    cities = data.cities.build_city_graph()
+
     def __init__(self, seed=None):
         self.seed = seed
         self.state = {"phase": "lobby"}
